30a0a6985a7296e7d0dba898f571a9bac2185b4c
Mosarraf Hosain <mosarraf.hosain.1987@gmail.com>
2019-07-20 14:28:52 +0600

    vue cli

---
+++ mamunur_rashid/vuecli/my-project/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-md-8 content-main">
+    <div class="content-grid">
+      <div class="content-grid-info">
+        <div class="post-info">
+          <h4>
+            <a
+              href="https://p.w3layouts.com/demos/personal_blog/web/single.html"
+            >About Us</a>
+            July 30, 2014 / 27 Comments
+          </h4>
+          <p>
+            This is about us page.
+          </p>
+          <a href="https://p.w3layouts.com/demos/personal_blog/web/single.html">
+            <span></span>READ MORE
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ mamunur_rashid/vuecli/my-project/src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="col-md-8 content-main">
+    <div class="content-grid">
+      <div class="content-grid-info">
+        <img src="/post1.jpg" alt />
+        <div class="post-info">
+          <h4>
+            <a
+              href="https://p.w3layouts.com/demos/personal_blog/web/single.html"
+            >Lorem ipsum dolor sit amet</a>
+            July 30, 2014 / 27 Comments
+          </h4>
+          <p>
+            Praesent dapibus, neque id cursus faucibus, tortor neque
+            egestas augue, eu vulputate magna eros eu erat. Aliquam
+            erat volutpat. Nam dui mi, tincidunt quis.
+          </p>
+          <a href="https://p.w3layouts.com/demos/personal_blog/web/single.html">
+            <span></span>READ MORE
+          </a>
+        </div>
+      </div>
+      <!---300x250--->
+      <div class="content-grid-info">
+        <img src="/post2.jpg" alt />
+        <div class="post-info">
+          <h4>
+            <a
+              href="https://p.w3layouts.com/demos/personal_blog/web/single.html"
+            >Lorem ipsum dolor sit amet</a>
+            July 30, 2014 / 27 Comments
+          </h4>
+          <p>
+            Praesent dapibus, neque id cursus faucibus, tortor neque
+            egestas augue, eu vulputate magna eros eu erat. Aliquam
+            erat volutpat. Nam dui mi, tincidunt quis.
+          </p>
+          <a href="https://p.w3layouts.com/demos/personal_blog/web/single.html">
+            <span></span>READ MORE
+          </a>
+        </div>
+      </div>
+      <div class="content-grid-info">
+        <img src="/post3.jpg" alt />
+        <div class="post-info">
+          <h4>
+            <a
+              href="https://p.w3layouts.com/demos/personal_blog/web/single.html"
+            >Lorem ipsum dolor sit amet</a>
+            July 30, 2014 / 27 Comments
+          </h4>
+          <p>
+            Praesent dapibus, neque id cursus faucibus, tortor neque
+            egestas augue, eu vulputate magna eros eu erat. Aliquam
+            erat volutpat. Nam dui mi, tincidunt quis.
+          </p>
+          <a href="https://p.w3layouts.com/demos/personal_blog/web/single.html">
+            <span></span>READ MORE
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
